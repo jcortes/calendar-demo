2c222d029c4ce07a8fece1c34a0c6cecc327b073
jcortes <jacortesmahmud@gmail.com>
2015-04-20 13:56:45 -0500

    testing bug

--- test/unit/appDirectiveSpec.js
+++ test/unit/appDirectiveSpec.js
@@ -13,11 +13,21 @@ describe('calendar', function(){
     }))
     
     it('should render the element correctly', function(){
-        var currentDate = new Date();
-        
+        var currDate = new Date();
+        var range = CalendarRange.getMonthlyRange(currDate);
         expect(element.find('select')[0].length).toBe(12);
         expect(element.find('select')[1].length).toBe(41);
-        expect(element.find('select option[selected=\'selected\']').eq(0).text()).toEqual(currentDate.toLocaleString('en-us', {month: 'long'}));
-        expect(element.find('select option[selected=\'selected\']').eq(1).text()).toEqual(currentDate.getFullYear().toString());        
+        expect(element.find('select option[selected=\'selected\']').eq(0).text()).toEqual(currDate.toLocaleString('en-us', {month: 'long'}));
+        expect(element.find('select option[selected=\'selected\']').eq(1).text()).toEqual(currDate.getFullYear().toString());
+        expect(element.find('.cal-cell').length).toBe(range.end.getDate());
+        expect(element.find('.cal-cell-out').length).toBe(range.days.length - range.end.getDate());
+    });
+    
+    it('should render the range correctly on April of 1995', function(){
+        var cusDate = new Date('1995', '3', '1');
+        var range = CalendarRange.getMonthlyRange(cusDate);
+        console.log(scope);
+        expect(element.find('select option[selected=\'selected\']').eq(0).text()).toEqual('April');
+        expect(element.find('select option[selected=\'selected\']').eq(1).text()).toEqual('1995');
     });
 })
